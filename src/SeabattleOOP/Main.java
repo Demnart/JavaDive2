4f45c52d73367ba60112681d946bf8f53e5815a9
Demnart <insidia93@gmail.com>
2016-09-16 12:56:40 +0300

    Добавлен класс Random для генерации случайных чисел. В строке 13-14 мы создаем переменные координат корабляж В строке 17 мы получаем случайное значение от 0 до 9 ж В строке 18 мы создаем обьект и передаем его конструктору полученное значение. В строке 19 мы передаем полю корабль. С 20 по 24 строчку мы выполняем цикл для того чтобы установить второй корабль при этом чтобы он не пересекался с 1.

--- src/SeabattleOOP/Main.java
+++ src/SeabattleOOP/Main.java
@@ -1,5 +1,7 @@
 package SeabattleOOP;
 
+import java.util.Random;
+
 /**
  * Created by Артем on 14.09.2016.
  */
@@ -7,9 +9,19 @@ public class Main {
     public static void main(String[] args) {
         Field field = new Field();
         Player player = new Player();
-        Ship ship = new Ship(4);
+        Random random = new Random();
+        int shipRandomCoordinate;
+        int shipRandomCoordinate1;
         field.initArr();
+
+        shipRandomCoordinate = random.nextInt(10);
+        Ship ship = new Ship(shipRandomCoordinate);
         field.setShip(ship);
+        do {
+            shipRandomCoordinate1 = random.nextInt(10);
+            Ship ship1 = new Ship(shipRandomCoordinate1);
+            field.setShip(ship1);
+        } while (shipRandomCoordinate == shipRandomCoordinate1);
 
         System.out.println("Игра началась");
 
